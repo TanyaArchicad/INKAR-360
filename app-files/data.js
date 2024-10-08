bb7951cb13c7019fea99940bc02ecd81a0bccc24
TanyaArchicad <tanyadiz2014@gmail.com>
2024-10-08 15:06:19 +0500

    Upload

---
+++ app-files/data.js
@@ -0,0 +1,661 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-prihozhaya",
+      "name": "Prihozhaya",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.528739917196912,
+        "pitch": 0.040939821960360945,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2937850430593585,
+          "pitch": -0.04973809639322013,
+          "rotation": 0,
+          "target": "1-vanna"
+        },
+        {
+          "yaw": -1.6130379277797502,
+          "pitch": 0.02801145713077169,
+          "rotation": 0,
+          "target": "2-gostevaya"
+        },
+        {
+          "yaw": -0.004429100403184449,
+          "pitch": -0.03248795786013403,
+          "rotation": 0,
+          "target": "4-kuhnya-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-vanna",
+      "name": "Vanna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.4325123258819428,
+        "pitch": 0.022221418341596078,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6167464627519035,
+          "pitch": -0.06528948314527838,
+          "rotation": 0,
+          "target": "0-prihozhaya"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-gostevaya",
+      "name": "Gostevaya",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5068829736999598,
+        "pitch": 0.022330669358948896,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4673473112390187,
+          "pitch": 0.06541585742539446,
+          "rotation": 0,
+          "target": "0-prihozhaya"
+        },
+        {
+          "yaw": 0.43146368633184373,
+          "pitch": -0.08179088227997688,
+          "rotation": 0,
+          "target": "3-lodziya-gostevaya"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-lodziya-gostevaya",
+      "name": "Lodziya Gostevaya",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.350382638320406,
+        "pitch": 0.12621462922076532,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8973479493404657,
+          "pitch": -0.20855335355339832,
+          "rotation": 0,
+          "target": "6-odziya-gostinya-"
+        },
+        {
+          "yaw": -2.7717700034866297,
+          "pitch": 0.02480257787972917,
+          "rotation": 0,
+          "target": "2-gostevaya"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kuhnya-1",
+      "name": "Kuhnya 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.1848319780348273,
+        "pitch": 0.01077363735799075,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.743303711744719,
+          "pitch": -0.3395446470119978,
+          "rotation": 0,
+          "target": "0-prihozhaya"
+        },
+        {
+          "yaw": -2.7263389623552925,
+          "pitch": 0.09428630687107464,
+          "rotation": 0,
+          "target": "1-vanna"
+        },
+        {
+          "yaw": -2.2491299555285593,
+          "pitch": 0.021215342295896278,
+          "rotation": 0,
+          "target": "2-gostevaya"
+        },
+        {
+          "yaw": -0.8964596507717442,
+          "pitch": -0.07930144917377469,
+          "rotation": 0,
+          "target": "6-odziya-gostinya-"
+        },
+        {
+          "yaw": -0.832831681287999,
+          "pitch": 0.2957932289536629,
+          "rotation": 0,
+          "target": "5-gostinya-1"
+        },
+        {
+          "yaw": -0.4593078292233983,
+          "pitch": 0.039390409410899,
+          "rotation": 0,
+          "target": "8-master-spalnya-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-gostinya-1",
+      "name": "Gostinya 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.8114320405634654,
+        "pitch": 0.00646418241479374,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6062573608429958,
+          "pitch": -0.28093035838905855,
+          "rotation": 0,
+          "target": "0-prihozhaya"
+        },
+        {
+          "yaw": 0.632283855762072,
+          "pitch": 0.01985784671246904,
+          "rotation": 0,
+          "target": "1-vanna"
+        },
+        {
+          "yaw": -0.341552820024809,
+          "pitch": -0.1649088061658368,
+          "rotation": 0,
+          "target": "4-kuhnya-1"
+        },
+        {
+          "yaw": 2.7332116295518762,
+          "pitch": -0.2037820699990398,
+          "rotation": 0,
+          "target": "6-odziya-gostinya-"
+        },
+        {
+          "yaw": -2.7792343182496886,
+          "pitch": -0.030710298014001935,
+          "rotation": 0,
+          "target": "8-master-spalnya-1"
+        },
+        {
+          "yaw": 2.1329391050596307,
+          "pitch": -0.0356987826224735,
+          "rotation": 0,
+          "target": "7-gostinya-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-odziya-gostinya-",
+      "name": "odziya Gostinya ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3542227316438904,
+        "pitch": 0.0009012675320221319,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9429984354318055,
+          "pitch": -0.28762641112815857,
+          "rotation": 0,
+          "target": "3-lodziya-gostevaya"
+        },
+        {
+          "yaw": -0.6886250135083607,
+          "pitch": 0.012425546471423132,
+          "rotation": 0,
+          "target": "7-gostinya-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-gostinya-2",
+      "name": "Gostinya 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5856530969362481,
+        "pitch": -0.03663036701716571,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6951807922479718,
+          "pitch": -0.07457236185236837,
+          "rotation": 0,
+          "target": "4-kuhnya-1"
+        },
+        {
+          "yaw": 1.982362171142391,
+          "pitch": 0.47670143337355597,
+          "rotation": 0,
+          "target": "5-gostinya-1"
+        },
+        {
+          "yaw": 2.079071851426799,
+          "pitch": -0.0013157698902404036,
+          "rotation": 0,
+          "target": "0-prihozhaya"
+        },
+        {
+          "yaw": 0.2674157871488063,
+          "pitch": 0.12332941158699917,
+          "rotation": 0,
+          "target": "8-master-spalnya-1"
+        },
+        {
+          "yaw": -1.7496164486803067,
+          "pitch": 0.20995854783550527,
+          "rotation": 0,
+          "target": "6-odziya-gostinya-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-master-spalnya-1",
+      "name": "Master Spalnya 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5861956322522701,
+        "pitch": -0.015460243413695451,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1686347138438133,
+          "pitch": 0.07322528012608664,
+          "rotation": 0,
+          "target": "7-gostinya-2"
+        },
+        {
+          "yaw": 1.9699878506944577,
+          "pitch": 0.004466836225903492,
+          "rotation": 0,
+          "target": "9-master-spalnya-2"
+        },
+        {
+          "yaw": 2.319956827940265,
+          "pitch": 0.001556994057494876,
+          "rotation": 0,
+          "target": "10-garderob"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-master-spalnya-2",
+      "name": "Master Spalnya 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5352873359163617,
+        "pitch": 0.05171345931834992,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8822016011127047,
+          "pitch": 0.0382695440241001,
+          "rotation": 0,
+          "target": "8-master-spalnya-1"
+        },
+        {
+          "yaw": 0.39840597292843327,
+          "pitch": 0.050103280938897043,
+          "rotation": 0,
+          "target": "7-gostinya-2"
+        },
+        {
+          "yaw": -2.2264769154212463,
+          "pitch": 0.15440095265269704,
+          "rotation": 0,
+          "target": "10-garderob"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-garderob",
+      "name": "Garderob",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5625436320344859,
+        "pitch": 0.10558164610829657,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9336744913713311,
+          "pitch": 0.3896874668735357,
+          "rotation": 0,
+          "target": "11-dysh-1"
+        },
+        {
+          "yaw": -1.6421595897163037,
+          "pitch": 0.6890387925198258,
+          "rotation": 0,
+          "target": "9-master-spalnya-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-dysh-1",
+      "name": "Dysh 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.8221176638169077,
+        "pitch": 0.04196207629641968,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5243222637402036,
+          "pitch": -0.07831180676655336,
+          "rotation": 0,
+          "target": "12-dysh-2"
+        },
+        {
+          "yaw": -2.749794501479414,
+          "pitch": 0.1275332531299398,
+          "rotation": 0,
+          "target": "10-garderob"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-dysh-2",
+      "name": "Dysh 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.3442447941168325,
+        "pitch": 0.19392547244381397,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8585701971308541,
+          "pitch": 0.1713660493972995,
+          "rotation": 0,
+          "target": "11-dysh-1"
+        },
+        {
+          "yaw": 0.3145123550570652,
+          "pitch": 0.08660127621764957,
+          "rotation": 0,
+          "target": "10-garderob"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
